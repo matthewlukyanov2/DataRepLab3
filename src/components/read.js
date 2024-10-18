ce7ac06091bbd7fd16219ead2194d42eb8c7bb3d
unknown <brucelukyanov@gmail.com>
2024-10-18 10:31:19 +0000

    2(b)

--- src/components/read.js
+++ src/components/read.js
@@ -1,28 +1,39 @@
+import { useEffect } from "react";
+import axios from "axios";
 import Movies from "./movies";
 const Read = () => {
     const data = [
-        {
-          "Title": "Avengers: Infinity War",
-          "Year": "2018",
-          "imdbID": "tt4154756",
-          "Type": "movie",
-          "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-        },
-        {
-          "Title": "Captain America: Civil War",
-          "Year": "2016",
-          "imdbID": "tt3498820",
-          "Type": "movie",
-          "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-        },
-        {
-          "Title": "World War Z",
-          "Year": "2013",
-          "imdbID": "tt0816711",
-          "Type": "movie",
-          "Poster": "https://m.media-amazon.com/images/M/MV5BNDQ4YzFmNzktMmM5ZC00MDZjLTk1OTktNDE2ODE4YjM2MjJjXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
-        }
+       // {
+       // "Title": "Avengers: Infinity War",
+       // "Year": "2018",
+       // "imdbID": "tt4154756",
+       // "Type": "movie",
+       //  "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
+       // },
+        //{
+        //  "Title": "Captain America: Civil War",
+        // "Year": "2016",
+        // "imdbID": "tt3498820",
+        //  "Type": "movie",
+        // "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
+        //},
+        //{
+        // "Title": "World War Z",
+        // "Year": "2013",
+        //"imdbID": "tt0816711",
+        //"Type": "movie",
+        //"Poster": "https://m.media-amazon.com/images/M/MV5BNDQ4YzFmNzktMmM5ZC00MDZjLTk1OTktNDE2ODE4YjM2MjJjXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
+        //}
       ];
+      useEffect(
+        ()=>{
+           axios.get('https://jsonblob.com/api/jsonblob/1287718524221775872')
+           .then(()=>{
+            
+           })
+           .catch()
+        }
+      );
 
     return (
         <div>
